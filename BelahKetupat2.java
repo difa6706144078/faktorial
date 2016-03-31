57a5b7305f815bc2ced214634574edad9f1da060
Muhammad Difa Irawan Djajus <difairawan24@gmail.com>
2016-03-31 11:10:48 +0700

    Added files via upload

---
+++ BelahKetupat2.java
@@ -0,0 +1,30 @@
+package belahketupat;
+
+import java.util.Scanner;
+
+public class BelahKetupat2 {
+  public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Masukan tinggi belah ketupat: ");
+        int n=scn.nextInt();
+ 
+
+    for (int i = 1; i < n; i += 2) {
+      for (int j = 0; j < 4 - i / 2; j++)
+        System.out.print(" ");
+
+      for (int j = 0; j < i; j++)
+        System.out.print("*");
+        System.out.print("\n");
+    }
+
+    for (int i = n; i > 0; i -= 2) {
+      for (int j = 0; j < 4 - i / 2; j++)
+        System.out.print(" ");
+
+      for (int j = 0; j < i; j++)
+        System.out.print("*");
+        System.out.print("\n");
+    }
+  }
+}
